e39657823733a0bb4a5cebbe2402317a66f99610
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2025-04-22 20:54:37 -0400

    feat: adds SQLite as appointments source

---
+++ lib/data/local/sqlite/db.dart
@@ -0,0 +1,26 @@
+import 'dart:io' as io;
+
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+class SqliteDB {
+  static Database? _instance;
+
+  static Future<Database> get instance async {
+    _instance ??= await SqliteDB._openSQLite();
+    return _instance!;
+  }
+
+  static Future<Database> _openSQLite() async {
+    sqfliteFfiInit();
+
+    var databaseFactory = databaseFactoryFfi;
+    final io.Directory appDocumentsDir =
+        await getApplicationDocumentsDirectory();
+    String dbPath = p.join(appDocumentsDir.path, "SalusGym", "salus_gym.db");
+
+    var db = await databaseFactory.openDatabase(dbPath);
+    return db;
+  }
+}
--- lib/schedule/appointment.dart
+++ lib/schedule/appointment.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:salus_gym/core/utils/color_parser.dart';
 
 class Appointment {
+  final String id;
   final String title;
   final DateTime startTime;
-  final DateTime endTime;
+  final int durationInMinutes;
   final Color color;
 
   Appointment({
+    required this.id,
     required this.title,
     required this.startTime,
-    required this.endTime,
-    this.color = Colors.blue,
+    required this.durationInMinutes,
+    required this.color,
   });
+
+  DateTime get endTime => startTime.add(Duration(minutes: durationInMinutes));
+
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'start_time_utc': DateFormat('yyyy-MM-ddTHH:mm').format(startTime),
+      'duration_in_minutes': durationInMinutes,
+      'color': ColorParser.toARGBString(color),
+    };
+  }
 }
