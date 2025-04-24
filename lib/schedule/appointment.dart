8e0c08efd02cd9642e0d748cf7f47825dafc11d1
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2025-04-24 01:36:44 -0400

    feat: makes date filter to work together with SQLite

--- lib/schedule/appointment.dart
+++ lib/schedule/appointment.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:salus_gym/core/utils/color_parser.dart';
 
 class Appointment {
   final String id;
   final String title;
-  final DateTime startTime;
+  final DateTime startTimeUtc;
   final int durationInMinutes;
   final Color color;
 
   Appointment({
     required this.id,
     required this.title,
-    required this.startTime,
+    required this.startTimeUtc,
     required this.durationInMinutes,
     required this.color,
   });
 
-  DateTime get endTime => startTime.add(Duration(minutes: durationInMinutes));
+  DateTime get startTimeLocal => startTimeUtc.toLocal();
+  DateTime get endTimeUtc =>
+      startTimeUtc.add(Duration(minutes: durationInMinutes));
+  DateTime get endTimeLocal => endTimeUtc.toLocal();
 
   Map<String, Object?> toMap() {
     return {
       'id': id,
       'title': title,
-      'start_time_utc': DateFormat('yyyy-MM-ddTHH:mm').format(startTime),
+      'start_time_utc': startTimeUtc.millisecondsSinceEpoch,
       'duration_in_minutes': durationInMinutes,
       'color': ColorParser.toARGBString(color),
     };
