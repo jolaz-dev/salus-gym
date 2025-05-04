8385b325a08ba053f1089aedf600cab15fca035f
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2025-05-03 20:48:57 -0400

    chore: updates icons and changes main window name

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:salus_gym/app.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await WindowManager.instance.ensureInitialized();
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitle('Salus Gym');
+  });
   initializeDateFormatting('pt_BR', null);
   runApp(const MyApp());
 }
